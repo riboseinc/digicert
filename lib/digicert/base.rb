7ec6419fa67550d62bdaaced6331ae6f6523171e
Abu Nashir <abunashir@gmail.com>
2017-02-18 16:30:12 +0700

    Let's be declarative for class methods
    In our API Resources, we are providing an easier interface to
    call it's instance methods using the class level methods, and
    we are using `method_missing` as fallback, and then it would
    check if that method has been defined as public method or not
    and if it did then it would create a new instance and invoke
    that method, But that has some potential drawbacks.
    
    So, after looking into that we only need `two/three` interface
    so, let's be declarative about those and define those method
    explicitly, it also ensures everything works as before.

--- lib/digicert/base.rb
+++ lib/digicert/base.rb
@@ -12,16 +12,18 @@ module Digicert
       response[resources_key]
     end
 
-    def fetch(name_id)
-      Digicert::Request.new(:get, [resource_path, name_id].join("/")).run
+    def fetch
+      Digicert::Request.new(
+        :get, [resource_path, @resource_id].join("/"),
+      ).run
     end
 
-    def self.method_missing(method_name, *arguments, &block)
-      if new.respond_to?(method_name, include_private: false)
-        new.send(method_name, *arguments, &block)
-      else
-        super
-      end
+    def self.all
+      new.all
+    end
+
+    def self.fetch(resource_id)
+      new(resource_id: resource_id).fetch
     end
 
     private
--- lib/digicert/certificate_request.rb
+++ lib/digicert/certificate_request.rb
@@ -3,16 +3,24 @@ require "digicert/base"
 module Digicert
   class CertificateRequest < Digicert::Base
 
-    def update(request_id, attributes)
+    def update
       Digicert::Request.new(
-        :put, [resource_path, request_id, "status"].join("/"), attributes,
+        :put, update_status_path, attributes,
       ).run
+    end
+
+    def self.update(resource_id, attributes)
+      new(resource_id: resource_id, **attributes).update
     end
 
     private
 
     def resource_path
       "request"
+    end
+
+    def update_status_path
+      [resource_path, resource_id, "status"].join("/")
     end
   end
 end
