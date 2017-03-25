3e12c9cd8f39a95b36e36c1ed60e82688a2afa31
Abu Nashir <abunashir@gmail.com>
2017-03-25 17:36:25 +0700

    Request spec for client email security plus
    This commit will adds the request specs for email security plus, but
    this seems to be causing the same issue as we have with the client
    premium certificate, I assume it could be something with the client
    certificate or something.

---
+++ spec/requests/order_client_email_security_plus_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper"
+
+RSpec.describe "Order client email security plus" do
+  describe "ordering client email security plus", api_call: true do
+    it "creates a new order for client email security plus" do
+      product_name_id = "email_security_plus"
+
+      # Reqeust a new email security plus using the order creation
+      # interface with `product_name_id` and required attributes
+      #
+      order_request = Digicert::Order.create(
+        product_name_id, order_attributes,
+      )
+
+      puts order_request
+    end
+  end
+
+  def ribose_inc
+    @ribose_inc ||= Digicert::Organization.all.first
+  end
+
+  def order_attributes
+    {
+      validity_years: 3,
+      certificate: certificate_attributes,
+      organization: { id: ribose_inc.id },
+    }
+  end
+
+  def certificate_attributes
+    {
+      common_name: "John Doe",
+      signature_hash: "sha256",
+      emails: ["johndoe@ribosetest.com"],
+    }
+  end
+end
