863565452052de3aad095bbc6e1bb37356fa77d0
Francesco <francesco.malvezzi@unimore.it>
2018-09-19 13:32:19 +0200

    add ssl_multi_domain

---
+++ lib/digicert/ssl_certificate/ssl_multi_domain.rb
@@ -0,0 +1,14 @@
+require "digicert/ssl_certificate/base"
+
+module Digicert
+  module SSLCertificate
+    class SSLMultiDomain < Digicert::SSLCertificate::Base
+      private
+
+      def certificate_type
+        "ssl_multi_domain"
+      end
+
+    end
+  end
+end
